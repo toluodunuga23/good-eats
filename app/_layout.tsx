a76c2b9285ad2af95deb8f803315109f73b00748
kawe-education <to379@drexel.edu>
2025-12-27 13:42:29 -0500

    layout setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
-
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 export default function RootLayout() {
-  return <Stack />;
+  return(
+    <>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="profile" options={{ headerShown: true, title: "Profile" }} />
+    </Stack>
+    </SafeAreaView>
+    </SafeAreaProvider>
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+  Image,
+
+} from "react-native";
+import { Link } from "expo-router";
+import { styles } from "../styles/home.styles";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.123</Text>
+    <View style={styles.container}>
+      {/* <Text style={styles.text}>Hello World</Text> */}
+      <Link href="/profile">
+        <Text style={styles.text}>Profile</Text>
+      </Link>
     </View>
+
   );
 }
+
+{
+  /* Image is a component that displays an image */
+}
+{
+  /* <Image source={require("../assets/images/icon.png")} style={styles.image} /> */
+}
+
+{
+  /* TouchableOpacity is a button that can be pressed */
+}
+{
+  /* <TouchableOpacity onPress={() => alert("Hello World")}>
+      <Text style={styles.text}>Click me</Text>
+      </TouchableOpacity> */
+}
+
+{
+  /* Pressable is a button that can be pressed */
+}
+{
+  /* <Pressable onPress={() => alert("Hello World")}>
+      <Text style={styles.text}>Click me</Text>
+      </Pressable> */
+}
+
